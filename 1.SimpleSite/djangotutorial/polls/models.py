b28a3aa4a88e398e02223d2f7438264b68bb2675
novakvova <novakvova@gmail.com>
2025-06-06 15:32:40 +0300

    Add custom user model with image field and unique email; update URL patterns for media file serving

--- 1.SimpleSite/djangotutorial/polls/models.py
+++ 1.SimpleSite/djangotutorial/polls/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    image = models.ImageField(upload_to='users/', null=True, blank=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
 
 # Create your models here.
 class Question(models.Model):
