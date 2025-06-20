9fa4cee0cab3025d04a016c2b962953b5b37663b
novakvova <novakvova@gmail.com>
2025-06-20 16:50:57 +0300

    Is good working admin panel

--- 2.RestAPI/my-react-app/src/App.tsx
+++ 2.RestAPI/my-react-app/src/App.tsx
@@ -3,6 +3,8 @@ import HomePage from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout";
 import * as React from "react";
+import AdminLayout from "./layout/admin/AdminLayout.tsx";
+import AdminDashboardPage from "./pages/admin/Dashboard";
 
 const App : React.FC = () => {
 
@@ -12,6 +14,13 @@ const App : React.FC = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
                 </Route>
+
+                <Route path={"admin"} element={<AdminLayout />}>
+
+                    <Route path="home" element={<AdminDashboardPage />}/>
+
+                </Route>
+
             </Routes>
         </>
     )
--- 2.RestAPI/my-react-app/src/main.tsx
+++ 2.RestAPI/my-react-app/src/main.tsx
@@ -4,11 +4,20 @@ import App from './App.tsx'
 import {Provider} from 'react-redux'
 import {store} from './store'
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "./context/ThemeContext.tsx";
+import {AppWrapper} from "./components/common/PageMeta.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>,
+    <>
+        <ThemeProvider>
+            <AppWrapper>
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <App/>
+                    </BrowserRouter>
+                </Provider>
+            </AppWrapper>
+        </ThemeProvider>
+    </>
+    ,
 )
